e0f4ad7e1fae55f6d685fc76a53343e4cd2dc2c0
Antonioluis92 <74476@student.dorset-college.ie>
2025-09-18 15:20:48 +0100

    CarDetail

---
+++ fs-b-2025-blazor-wk1-74476/Components/Models/Cars.cs
@@ -0,0 +1,9 @@
+﻿public class Car
+{
+    public int Id { get; set; }
+    public string Model { get; set; }
+    public string Brand { get; set; }
+    public int Year { get; set; }
+    public string Type { get; set; }
+    public double Rating { get; set; }
+}
---
+++ fs-b-2025-blazor-wk1-74476/Components/Repositories/CarRepository.cs
@@ -0,0 +1,14 @@
+﻿public static class CarRepository
+{
+    public static List<Car> GetCars()
+    {
+        return new List<Car>
+        {
+            new Car { Id = 1, Model = "Mustang", Brand = "Ford", Year = 2020, Type = "Sports", Rating = 8.7 },
+            new Car { Id = 2, Model = "Civic", Brand = "Honda", Year = 2019, Type = "Sedan", Rating = 8.2 },
+            new Car { Id = 3, Model = "Model S", Brand = "Tesla", Year = 2021, Type = "Electric", Rating = 9.1 },
+            new Car { Id = 4, Model = "Corolla", Brand = "Toyota", Year = 2018, Type = "Sedan", Rating = 7.9 },
+            new Car { Id = 5, Model = "Wrangler", Brand = "Jeep", Year = 2022, Type = "SUV", Rating = 8.5 }
+        };
+    }
+}
